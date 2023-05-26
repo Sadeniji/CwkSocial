20be4f79d899a06adb4c54a3097b16e940ff197d
Saheed Adeniji <adeniji4success@gmail.com>
2023-05-26 17:11:36 +0100

    Add project files.

---
+++ CwkSocial.Api/Controllers/PostsController.cs
@@ -0,0 +1,23 @@
+﻿using CwkSocial.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CwkSocial.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var post = new Post
+            {
+                Id = id,
+                Text = "Hello World!"
+            };
+
+            return Ok(post);
+        }
+    }
+}
---
+++ CwkSocial.Domain/Models/Post.cs
@@ -0,0 +1,7 @@
+﻿namespace CwkSocial.Domain.Models;
+
+public class Post
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; }   = string.Empty;
+}
